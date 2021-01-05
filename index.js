f0a0536c9182df1dc68ead37d25e7b93d8641376
Ani Srikanth <ani.srikanth23@gmail.com>
2021-01-05 00:01:08 -0500

    Update Ping/Pong

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http, {
-    pingTimeout: 3600000,
-    pingInterval: 3600000
+    pingTimeout: 3600000
 });
 
 rooms = {
